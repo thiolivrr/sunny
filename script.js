379899b439f733c860309215efd43909a84e51eb
Ac4stus <thiago.oliv0101@outlook.com>
2023-04-17 21:24:17 -0300

    Lacks a Menu Yet

---
+++ script.js
@@ -0,0 +1,7 @@
+var nav = document.getElementsByClassName('navbar');
+var ham = document.getElementsByClassName('hamburguer');
+ham.addEventListener("click", ShowMenu);
+
+function ShowMenu() {
+    window.alert("fds");
+}
