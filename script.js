c38b00df8820fca26ce3bbdb0c7d49818995bd45
Ac4stus <thiago.oliv0101@outlook.com>
2023-04-21 20:04:54 -0300

    hamburguer to close

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function ShowMenu() {
-    navbar.style.display = 'flex';
-}
+    var nav = document.querySelector('.navbar')
+    if (nav.style.display == 'flex') {
+      nav.style.display = 'none'
+      document.querySelector('.ham_img').src = "images/icon-hamburger.svg"
+    } else {
+      nav.style.display = 'flex'
+      document.querySelector('.ham_img').src = "images/close2.png"
+    }    
+  }
